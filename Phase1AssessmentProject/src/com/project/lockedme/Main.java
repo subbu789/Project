ecec526219ffc59fc48a6d67c3e110adf15d18e9
subbu789 <99788326+subbu789@users.noreply.github.com>
2022-03-06 21:01:31 +0530

    Add files via upload

---
+++ Phase1AssessmentProject/src/com/project/lockedme/Main.java
@@ -0,0 +1,14 @@
+package com.project.lockedme;
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\subhani shaik\\eclipse-workspace"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+
+}
+}
